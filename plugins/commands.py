15a4ef827b4fba24231d1e15cb2bffbe530c0551
W <73099288+wertinium@users.noreply.github.com>
2021-09-19 10:20:58 +0400

    CMD

---
+++ plugins/commands.py
@@ -0,0 +1,32 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from .info import get_movie
+
+START_TEXT = """Hello {}
+I am a movie information finder bot.
+> `I can find information of all movies.`
+Made by @FayasNoushad"""
+
+JOIN_BUTTONS = [
+    InlineKeyboardButton(
+        text='Blog Kanalıma katılın',
+        url='https://telegram.me/wertinium'
+    )
+]
+
+BUTTONS = InlineKeyboardMarkup(
+    [JOIN_BUTTONS]
+)
+
+@Client.on_message(filters.private & filters.command(["start"]), group=-1)
+async def start(bot, update):
+    if update.text == "/start":
+        await update.reply_text(
+            text=START_TEXT.format(update.from_user.mention),
+            reply_markup=BUTTONS,
+            disable_web_page_preview=True,
+            quote=True
+        )
+    else:
+        movie = update.text.split(" ", 1)[1]
+        await get_movie(bot, update, movie)
