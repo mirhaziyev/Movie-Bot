d0afb61fbc9a866393d88f60fb0c6aa6c7466093
W <73099288+wertinium@users.noreply.github.com>
2021-09-19 10:23:23 +0400

    Create callback.py

---
+++ plugins/callback.py
@@ -0,0 +1,10 @@
+from pyrogram import Client
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from plugins.info import *
+
+
+@Client.on_callback_query()
+async def callback(bot, update):
+    data = update.data
+    if data == "close":
+        update.message.delete()
